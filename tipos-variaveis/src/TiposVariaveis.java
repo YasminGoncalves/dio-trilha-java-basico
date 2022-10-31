3cbc3ac3cebb1ba10d2be4e084e155a8f1708e91
YasminGoncalves <yasmingoncalvesrodrigues@hotmail.com>
2022-10-30 21:29:19 -0300

    Exemplos de tipos primitivos de dados.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 6;
+        numero = 10;
+
+        System.out.print(numero);
+
+        // Para garantir que o valor não poderá ser modificado.
+        final double VALOR_DE_PI = 3.14;
     }
 }
